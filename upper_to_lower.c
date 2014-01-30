a7e6e8c0e0fcf5d7956d21684be743e01d090cbc
Justus Goldstein-Shirley <goldstei4@grinnell.edu>
2014-01-30 00:11:04 -0600

    Function that, given filename, converts all characters to lowercase. Working.

---
+++ upper_to_lower.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+void upper_to_lower(char* name){
+  FILE *file;
+  file = fopen(name, "r");
+  if(file == NULL){
+    perror("No such file.");
+  }else{
+
+    //Find length of file [1]
+    int length;
+    fpos_t position;
+    fgetpos( file, &position );
+    fseeko( file, 0, SEEK_END );
+    length = ftello( file );
+    fsetpos( file, &position );
+
+    //Save contents of file to array as lowercase
+    char contents[length];
+    int i;
+    for(i=0; i<length; i++){
+      contents[i] = tolower(fgetc(file));
+    }
+    fclose(file);
+
+    //Write contents back into file
+    file = fopen(name, "w");
+    for(i=0; i<length; i++){
+      fputc(contents[i], file);
+    }
+  }
+  fclose(file);
+}
+
+int main(){
+  upper_to_lower("test.txt");
+  return 0;
+}
+
+/*
+[1] The code for finding the length of the file was copied wholesale from StackOverflow user Potatoswatter's answer to fang_dejavu's question "Counting unknown numbers of chars from a file in C", found <http://stackoverflow.com/questions/2645598/counting-unknown-numbers-of-chars-from-a-file-in-c> and accessed 1/29/14.
+
+[2] Help with basic file i/o came from "Intro to File Input/Output in C" at <http://www.cs.bu.edu/teaching/c/file-io/intro/>, accessed 1/29/14.
+*/
